dbbe8cc2f772ec75e1e0105b9aafac682bb27b74
Stefan Dili <stefan.dili.021@gmail.com>
2020-05-01 11:53:55 +0200

    made and tested comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :body, presence: true, length: {maximum: 300}
+  validates :user, presence: true
+  validates :post, presence: true
+  
 end
